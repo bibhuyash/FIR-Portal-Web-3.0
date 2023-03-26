c8b132d193b7ef71b3e88d5e81a3f2c77ff629aa
bibhuyash <vibhakaryashaswi@gmail.com>
2023-03-26 10:44:44 +0530

    Done the initial setup of client folder

---
+++ client/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function FIRPortal() {
+  return (
+    <>
+      <Head>
+        <title>FIR Portal Web3.0</title>
+        <meta name="description" content="Decentralized FIR Portal" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
