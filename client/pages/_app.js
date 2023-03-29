5c3088c355976010d42032971ddfc814556e18b0
bibhuyash <vibhakaryashaswi@gmail.com>
2023-03-29 16:19:17 +0530

    Added thirdwebProvider in app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Toaster } from 'react-hot-toast';
+import { Mumbai, Polygon } from "@thirdweb-dev/chains";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider activeChain={Mumbai}
+      chains={[Polygon, Mumbai]}>
+      <Component {...pageProps} />
+      <Toaster />
+    </ThirdwebProvider>
+  )
 }
