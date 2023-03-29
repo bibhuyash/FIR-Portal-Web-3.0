7c0e23116ed97820baefece401ca072789eb35f7
bibhuyash <vibhakaryashaswi@gmail.com>
2023-03-29 16:19:55 +0530

    Added Header and Complaint component in index.js file

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
+import Header from './components/Header';
+import Complaint from './components/Complaint';
 
 export default function FIRPortal() {
+  const address = useAddress();
   return (
     <>
       <Head>
@@ -12,9 +13,8 @@ export default function FIRPortal() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+      <Header />
+      <Complaint />
     </>
   )
 }
