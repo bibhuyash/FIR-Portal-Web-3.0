23363cb769552c679c569f815f7424006c9b8601
bibhuyash <vibhakaryashaswi@gmail.com>
2023-04-01 15:58:16 +0530

    Added Status and Admin Component in index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,9 +2,12 @@ import Head from 'next/head'
 import { useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
 import Header from './components/Header';
 import Complaint from './components/Complaint';
+import Admin from './components/Admin';
+import Status from './components/Status';
 
 export default function FIRPortal() {
   const address = useAddress();
+  const { contract } = useContract(process.env.NEXT_PUBLIC_SMART_CONTRACT);
   return (
     <>
       <Head>
@@ -15,6 +18,8 @@ export default function FIRPortal() {
       </Head>
       <Header />
       <Complaint />
+      <Status />
+      <Admin />
     </>
   )
 }
